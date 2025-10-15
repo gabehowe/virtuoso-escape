a9201d3de7531e7199513998e4903dcaff09c602
treasureuz <uzondut@gmail.com>
2025-10-15 18:39:54 -0400

    tested login; added test toString in Account; tried fixing AccountManager's login;  changed .language(..) to .string(..) in Entity.

--- src/main/java/org/virtuoso/escape/model/Entity.java
+++ src/main/java/org/virtuoso/escape/model/Entity.java
@@ -4,63 +4,79 @@ import org.virtuoso.escape.model.actions.Action;
 import org.virtuoso.escape.model.actions.TakeInput;
 
 public class Entity {
-    private String id;
-    private Action attackAction;
-    private Action inspectAction;
-    private Action interactAction;
-    private TakeInput inputAction;
+	private String id;
+	private Action attackAction;
+	private Action inspectAction;
+	private Action interactAction;
+	private TakeInput inputAction;
 
-    public Entity(String id, Action attackAction, Action inspectAction, Action interactAction) {
-        this.id = id;
-        this.attackAction = attackAction;
-        this.inspectAction = inspectAction;
-        this.interactAction = interactAction;
-    }
+	public Entity(String id, Action attackAction, Action inspectAction, Action interactAction) {
+		this.id = id;
+		this.attackAction = attackAction;
+		this.inspectAction = inspectAction;
+		this.interactAction = interactAction;
+	}
 
-    private String getText(String key) {
-        try {
-            return GameInfo.instance().string(this.id,key);
-        } catch (Exception e) {
-            return "[Missing text: " + key + "]";
-        }
-    }
+	private String getText(String key) {
+		try {
+			return GameInfo.instance().string(this.id, key);
+		} catch (Exception e) {
+			return "[Missing text: " + key + "]";
+		}
+	}
 
-    public void interact() {
-        GameState.instance().setCurrentMessage(getText("interact"));
-        if (interactAction != null) interactAction.execute();
-    }
+	public void interact(Item item) {
+		String message = null;
+		if (item != null) {
+			try {
+				message = GameInfo.instance().string(this.id, item.id());
+			} catch (Exception ignored) {
+			}
+		}
+		if (message == null) message = getText("interact");
 
-    public void attack() {
-        GameState.instance().setCurrentMessage(getText("attack"));
-        if (attackAction != null) attackAction.execute();
-    }
+		GameState.instance().setCurrentMessage(message);
 
-    public void inspect() {
-        GameState.instance().setCurrentMessage(getText("inspect"));
-        if (inspectAction != null) inspectAction.execute();
-    }
+		if (interactAction != null) {
+			interactAction.execute();
+		}
+	}
 
-    public String name() {
-        return GameInfo.instance().string(this.id, "name");
-    }
+	public void attack() {
+		GameState.instance().setCurrentMessage(getText("attack"));
+		if (attackAction != null) attackAction.execute();
+	}
 
-    public void introduce() {
-        GameState.instance().setCurrentMessage(getText("introduce"));
-    }
+	public void inspect() {
+		GameState.instance().setCurrentMessage(getText("inspect"));
+		if (inspectAction != null) inspectAction.execute();
+	}
 
-    public boolean equals(Entity other) {
-        return this.id.equals(other.id);
-    }
+	public String name() {
+		return GameInfo.instance().language().get(this.id).get("name");
+	}
 
-    public String id() {
-        return this.id;
-    }
+	public void introduce() {
+		GameState.instance().setCurrentMessage(getText("introduce"));
+	}
 
-    public void takeInput(String input) {
-        String message = GameInfo.instance().string(this.id, "input_" + input);
-        if (message != null) GameState.instance().setCurrentMessage(this.name() + " doesn't respond.");
+	public boolean equals(Entity other) {
+		return this.id.equals(other.id);
+	}
 
-        if (inputAction != null) inputAction.withInput(input).execute();
-    }
+	public String id() {
+		return this.id;
+	}
+
+	public void takeInput(String input) {
+		String message = GameInfo.instance().string(this.id, input);
+		if (message != null) {
+			GameState.instance().setCurrentMessage(message);
+		}
+
+		if (inputAction != null) {
+			inputAction.withInput(input).execute();
+		}
+	}
 
 }
--- src/main/java/org/virtuoso/escape/model/data/dataTester.java
+++ src/main/java/org/virtuoso/escape/model/data/dataTester.java
@@ -1,22 +1,25 @@
 package org.virtuoso.escape.model.data;
 
+import java.util.HashMap;
+import java.util.Optional;
 
 import org.json.simple.JSONObject;
+import org.virtuoso.escape.model.GameProjection;
 import org.virtuoso.escape.model.GameState;
 import org.virtuoso.escape.model.account.Account;
 import org.virtuoso.escape.model.account.AccountManager;
 
 public class dataTester {
-    private static DataLoader dl;
-    private static JSONObject states;
-    private static AccountManager am;
+	private static DataLoader dl;
+	private static JSONObject states;
+	private static AccountManager am;
 
-    public static void main(String args[]){
-		GameState gs = GameState.instance();
-		gs.setCurrentMessage("Hello");
-		DataWriter.writeGameState();
-
-    }
-    
-
+	static void main(String[] args) {
+//		GameState gs = GameState.instance();
+//		gs.setCurrentMessage("Hello");
+//		DataWriter.writeGameState();
+		am = AccountManager.instance();
+		GameProjection gP = new GameProjection();
+		//System.out.println(gP.login("JsonLaquermelonie", "dog"));
+	}
 }
