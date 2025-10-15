620962fe10da65e28e6881e91050a1fc1e7e4027
Bose Kaikini <bose.kaikini@gmail.com>
2025-10-15 08:04:01 -0400

    Updated Entity.java - takeinput

--- src/main/java/org/virtuoso/escape/model/Entity.java
+++ src/main/java/org/virtuoso/escape/model/Entity.java
@@ -1,54 +1,73 @@
 package org.virtuoso.escape.model;
 
-import java.util.function.BooleanSupplier;
-
 import org.virtuoso.escape.model.actions.Action;
 import org.virtuoso.escape.model.actions.TakeInput;
 
 public class Entity {
-	private String id;
-	private Action attackAction;
-	private Action inspectAction;
-	private Action interactAction;
-	private TakeInput inputAction;
+    private String id;
+    private Action attackAction;
+    private Action inspectAction;
+    private Action interactAction;
+    private TakeInput inputAction;
 
-	public Entity(String id, Action attackAction, Action inspectAction, Action interactAction) {
-		this.id = id;
-		this.attackAction = attackAction;
-		this.inspectAction = inspectAction;
-		this.interactAction = interactAction;
-	}
+    public Entity(String id, Action attackAction, Action inspectAction, Action interactAction) {
+        this.id = id;
+        this.attackAction = attackAction;
+        this.inspectAction = inspectAction;
+        this.interactAction = interactAction;
+    }
 
-	public void interact(Item item) {
+    private String getText(String key) {
+        try {
+            return GameInfo.getInstance().getLanguage(this.id).get(key);
+        } catch (Exception e) {
+            return "[Missing text: " + key + "]";
+        }
+    }
 
-	}
-	
-	public void attack() {
-		GameState.instance().setCurrentMessage(GameInfo.instance().string(this.id, "attack"));
-	}
+    public void interact(Item item) {
+        String message = null;
+        if (item != null) {
+            try {
+                message = GameInfo.getInstance().getLanguage(this.id).get(item.id());
+            } catch (Exception ignored) {}
+        }
+        if (message == null) message = getText("interact");
 
-	public void inspect() {
-		GameState.instance().setCurrentMessage(GameInfo.instance().string(this.id, "inspect"));
-	}
+        GameState.instance().setCurrentMessage(message);
 
-	public void introduce() {
-		GameState.instance().setCurrentMessage(GameInfo.instance().string(this.id, "introduce"));
-	}
+        if (interactAction != null) {
+            interactAction.execute();
+        }
+    }
 
-	public boolean equals(Entity other) {
-		return this.id.equals(other.id);
-	}
+    public void attack() {
+        GameState.instance().setCurrentMessage(getText("attack"));
+        if (attackAction != null) attackAction.execute();
+    }
 
-	public String id(){
-		return this.id;
-	}
+    public void inspect() {
+        GameState.instance().setCurrentMessage(getText("inspect"));
+        if (inspectAction != null) inspectAction.execute();
+    }
 
+    public void introduce() {
+        GameState.instance().setCurrentMessage(getText("introduce"));
+    }
 
-	public String name() {
-		return GameInfo.getInstance().getLanguage(this.id).get("name");
-	}
+    public boolean equals(Entity other) {
+        return this.id.equals(other.id);
+    }
 
-	public void takeInput(String input) {
-		this.inputAction.withInput(input).execute();
-	}
+    public String id() {
+        return this.id;
+    }
+
+    public String name() {
+        return GameInfo.getInstance().getLanguage(this.id).get("name");
+    }
+
+    public void takeInput(String input) {
+		??
+    }
 }
