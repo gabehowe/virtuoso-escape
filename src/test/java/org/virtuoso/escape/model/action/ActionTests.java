04c291352449017cea5c0e537ed75ea539c282b9
Gabriel Howe <me@gabe.how>
2025-10-28 15:20:26 -0400

    create tests for MOST actions; close #183, NOT #194; NOT #192

--- src/test/java/org/virtuoso/escape/model/action/ActionTests.java
+++ src/test/java/org/virtuoso/escape/model/action/ActionTests.java
@@ -3,23 +3,36 @@ package org.virtuoso.escape.model.action;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.TemplateInvocationValidationException;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.virtuoso.escape.model.Difficulty;
 import org.virtuoso.escape.model.GameProjection;
 import org.virtuoso.escape.model.GameState;
 import org.virtuoso.escape.model.data.DataLoader;
 
+import java.util.LinkedHashMap;
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ActionTests {
     GameProjection proj;
 
+    private static Stream<Arguments> addPenaltyTest() {
+        return Stream.of(
+                Arguments.of(Difficulty.VIRTUOSIC, Severity.HIGH, 5 * 3 * 3),
+                Arguments.of(Difficulty.VIRTUOSIC, Severity.MEDIUM, 5 * 3 * 2),
+                Arguments.of(Difficulty.VIRTUOSIC, Severity.LOW, 5 * 3 * 1),
+                Arguments.of(Difficulty.SUBSTANTIAL, Severity.HIGH, 5 * 2 * 3),
+                Arguments.of(Difficulty.SUBSTANTIAL, Severity.MEDIUM, 5 * 2 * 2),
+                Arguments.of(Difficulty.SUBSTANTIAL, Severity.LOW, 5 * 2 * 1),
+                Arguments.of(Difficulty.TRIVIAL, Severity.HIGH, 5 * 1 * 3),
+                Arguments.of(Difficulty.TRIVIAL, Severity.MEDIUM, 5 * 1 * 2),
+                Arguments.of(Difficulty.TRIVIAL, Severity.LOW, 5 * 1 * 1)
+        );
+    }
 
     @BeforeEach
     public void pre() {
@@ -44,18 +57,64 @@ public class ActionTests {
         assertEquals(GameState.instance().penalty(), expectedPenalty);
     }
 
-    private static Stream<Arguments> addPenaltyTest() {
-        return Stream.of(
-                Arguments.of(Difficulty.VIRTUOSIC, Severity.HIGH,     5 * 3 * 3),
-                Arguments.of(Difficulty.VIRTUOSIC, Severity.MEDIUM,   5 * 3 * 2),
-                Arguments.of(Difficulty.VIRTUOSIC, Severity.LOW,      5 * 3 * 1),
-                Arguments.of(Difficulty.SUBSTANTIAL, Severity.HIGH,   5 * 2 * 3),
-                Arguments.of(Difficulty.SUBSTANTIAL, Severity.MEDIUM, 5 * 2 * 2),
-                Arguments.of(Difficulty.SUBSTANTIAL, Severity.LOW,    5 * 2 * 1),
-                Arguments.of(Difficulty.TRIVIAL, Severity.HIGH,       5 * 1 * 3),
-                Arguments.of(Difficulty.TRIVIAL, Severity.MEDIUM,     5 * 1 * 2),
-                Arguments.of(Difficulty.TRIVIAL, Severity.LOW,        5 * 1 * 1)
-        );
+    @Test
+    public void chainTest() {
+        new Chain(new SetMessage("initial"), new SetMessage("expected")).execute();
+        assertEquals("expected", GameState.instance().currentMessage().get());
     }
+
+    @ParameterizedTest
+    @ValueSource(booleans = {true, false})
+    public void conditionalTest(boolean value) {
+        new Conditional(() -> value, new SetMessage(String.valueOf(true)), new SetMessage(String.valueOf(false))).execute();
+        assertEquals(String.valueOf(value), GameState.instance().currentMessage().get());
+    }
+    @Test
+    public void nullConditionalTest() {
+        new Conditional(() -> false, new SetMessage(String.valueOf(true)), null).execute();
+        assertFalse(GameState.instance().currentMessage().isPresent());
+    }
+
+    @Test
+    public void defaultTest() {
+        new Default().execute();
+        // Expect no error?
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings={"one", "two", "three"})
+    public void testTakeInput(String input) {
+        new TakeInput("one", new SetMessage("one"), "two", new SetMessage("two"), "three", new SetMessage("three")).withInput(input).execute();
+        var msg = GameState.instance().currentMessage();
+        assertTrue(msg.isPresent());
+        assertEquals(input, msg.get());
+    }
+    @Test
+    public void testDefaultTakeInput() {
+        new TakeInput("", new LinkedHashMap<>(), new SetMessage("default")).withInput(null).execute();
+        var msg = GameState.instance().currentMessage();
+        assertTrue(msg.isPresent());
+        assertEquals("default", msg.get());
+    }
+    @Test
+    public void testNullDefaultTakeInput() {
+        new TakeInput("", new LinkedHashMap<>(), null).withInput(null).execute();
+        var msg = GameState.instance().currentMessage();
+        assertFalse(msg.isPresent());
+    }
+
+    @Test
+    public void testNonEvenTakeInputMakeCasesArgumentLength() {
+        try {
+            new TakeInput("one").execute(); // no second!
+        }
+        catch (AssertionError | IndexOutOfBoundsException e){
+            // used the function wrong -- These errors SHOULD be thrown!
+            return;
+        }
+        fail();
+    }
+
+
 
 }
