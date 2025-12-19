c1d00ddbebcda6a1c2c497bdea680394e85f99bd
Gabriel Howe <me@gabe.how>
2025-12-19 00:38:23 -0500

    make skip clickable on mobile; close #13

--- src/jsMain/kotlin/Intro.kt
+++ src/jsMain/kotlin/Intro.kt
@@ -1,6 +1,10 @@
 import kotlinx.browser.document
+import kotlinx.html.dom.append
+import kotlinx.html.js.b
+import kotlinx.html.js.span
 import org.virtuoso.escape.model.GameProjection
 import org.w3c.dom.HTMLDivElement
+import org.w3c.dom.HTMLElement
 import org.w3c.dom.HTMLImageElement
 
 object Intro {
@@ -17,10 +21,32 @@ object Intro {
           style.opacity = "1"
           Game.setDialogue(Game.sanitizeForJS(projection.language.string("ui", "intro")), "overlay")
           this.lastElementChild!!.lastElementChild.asDynamic().onanimationend = {
-            document.getElementById("skip")!!.innerHTML = "" + "Press Q to continue..."
+            (document.getElementById("skip")!! as HTMLElement).apply {
+              innerHTML = ""
+              append {
+                span {
+                  +"["
+                }
+                b {
+                  +"P"
+                }.apply {
+                  style.textDecoration = "underline"
+                }
+                span {
+                  +"ress anywhere to continue]"
+                }
+              }
+            }
+            "Press anywhere to continue..."
+            document.addEventListener("click", { switchTo(View.GameView, projection) })
           }
         }
       }
+    }
+    (document.getElementById("skip")!! as HTMLElement).apply {
+      addEventListener("click", {
+        switchTo(View.GameView, projection)
+      })
     }
     barn.apply {
       onclick = {
@@ -31,7 +57,7 @@ object Intro {
       }
     }
     document.onkeypress = kp@{
-      if (it.key != "q") return@kp Unit
+      if (it.key != "p") return@kp Unit
       switchTo(View.GameView, projection)
     }
   }
